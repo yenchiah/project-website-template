18d77515295f351877c41ee60e52ee3fb917a348
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-09-30 17:38:05 -0400

    Add one example of text with centered image. Increase version number.

--- js/widgets.js
+++ js/widgets.js
@@ -1,6 +1,6 @@
 /*************************************************************************
  * GitHub: https://github.com/yenchiah/project-website-template
- * Version: v3.13
+ * Version: v3.14
  * This JS file has widgets for building interactive web applications
  * Use this file with widgets.css
  * If you want to keep this template updated, avoid modifying this file
