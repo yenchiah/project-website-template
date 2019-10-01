b1b877c065297abc41fc114fa336052132595930
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2019-10-01 15:13:22 -0400

    Add an example of iframe in the template file. Increase version number.

--- js/widgets.js
+++ js/widgets.js
@@ -1,6 +1,6 @@
 /*************************************************************************
  * GitHub: https://github.com/yenchiah/project-website-template
- * Version: v3.14
+ * Version: v3.15
  * This JS file has widgets for building interactive web applications
  * Use this file with widgets.css
  * If you want to keep this template updated, avoid modifying this file
