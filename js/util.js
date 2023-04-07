2368a0c2179eb5bb392ef3426c614d3d7bf118eb
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2023-04-07 23:16:38 +0200

    Fix a problem in the menu item clickable region. Increase version number.

--- js/util.js
+++ js/util.js
@@ -1,6 +1,6 @@
 /*************************************************************************
  * GitHub: https://github.com/yenchiah/project-website-template
- * Version: v3.38
+ * Version: v3.39
  * This JS file has general utility functions
  * Use this file with widgets.js
  * If you want to keep this template updated, avoid modifying this file
